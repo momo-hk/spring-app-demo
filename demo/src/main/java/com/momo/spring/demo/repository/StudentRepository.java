9b3d432b6a85741f7387f5edf2dba97bbcca66e8
Mo <yeungyiumo@edb.gov.hk>
2018-01-08 16:06:02 +0800

    Description: Remove redundant files

--- demo/src/main/java/com/momo/spring/demo/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.momo.spring.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.momo.spring.demo.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-	
-}
